b1573b8ebe59722ed53d30a03e4d220ea9cbb5eb
Johan Nenzén <johan.nanzen@gmail.com>
2016-11-07 08:25:33 +0100

    Updates to use Material Design and new view components

--- app/src/main/java/com/homeki/android/MainActivity.java
+++ app/src/main/java/com/homeki/android/MainActivity.java
@@ -2,123 +2,122 @@ package com.homeki.android;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends ActionBarActivity {
-	private DrawerLayout drawerLayout;
-	private ActionBarDrawerToggle drawerToggle;
-	private MenuOption selectedMenuItem;
-	private View devicesMenu;
-	private View actionGroupsMenu;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private MenuOption selectedMenuItem;
+    private NavigationView mNavigationView;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-		drawerLayout = (DrawerLayout) findViewById(R.id.main_layout);
-		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.app_name, R.string.app_name);
-		drawerLayout.setDrawerListener(drawerToggle);
-		drawerToggle.setDrawerIndicatorEnabled(true);
+        Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
 
-		ActionBar actionBar = getSupportActionBar();
-		actionBar.setDisplayShowCustomEnabled(true);
-		actionBar.setDisplayHomeAsUpEnabled(true);
-		actionBar.setHomeButtonEnabled(true);
+        if (getSupportActionBar() != null) {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
 
-		devicesMenu = findViewById(R.id.devices_menu);
-		actionGroupsMenu = findViewById(R.id.action_groups_menu);
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.app_name, R.string.app_name);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.setDrawerIndicatorEnabled(true);
 
-		devicesMenu.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				drawerLayout.closeDrawers();
-				selectedMenuItem = MenuOption.DEVICES;
-				updateFragment();
-			}
-		});
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
 
-		actionGroupsMenu.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				drawerLayout.closeDrawers();
-				selectedMenuItem = MenuOption.ACTION_GROUPS;
-				updateFragment();
-			}
-		});
+        selectedMenuItem = MenuOption.DEVICES;
+        if (savedInstanceState != null && savedInstanceState.containsKey("selectedMenuItem")) {
+            selectedMenuItem = MenuOption.valueOf(savedInstanceState.getString("selectedMenuItem"));
+        }
 
-		findViewById(R.id.settings_menu).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				drawerLayout.closeDrawers();
-				startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-			}
-		});
+        updateFragment();
+    }
 
-		selectedMenuItem = MenuOption.DEVICES;
-		if (savedInstanceState != null && savedInstanceState.containsKey("selectedMenuItem")) selectedMenuItem = MenuOption.valueOf(savedInstanceState.getString("selectedMenuItem"));
-		updateFragment();
-	}
+    private void updateFragment() {
+        switch (selectedMenuItem) {
+            case ACTION_GROUPS:
+                showActionGroups();
+                break;
+            case DEVICES:
+            default:
+                showDevices();
+                break;
+        }
+    }
 
-	private void updateFragment() {
-		switch (selectedMenuItem) {
-			case ACTION_GROUPS:
-				showActionGroups();
-				break;
-			case DEVICES:
-			default:
-				showDevices();
-				break;
-		}
-	}
+    private void showDevices() {
+        mNavigationView.setCheckedItem(R.id.menu_device);
+        changeFragment(new DeviceListFragment());
+        selectedMenuItem = MenuOption.DEVICES;
+    }
 
-	private void showDevices() {
-		devicesMenu.setSelected(true);
-		actionGroupsMenu.setSelected(false);
-		changeFragment(new DeviceListFragment());
-		selectedMenuItem = MenuOption.DEVICES;
-	}
+    private void showActionGroups() {
+        mNavigationView.setCheckedItem(R.id.menu_action_groups);
+        changeFragment(new ActionGroupListFragment());
+        selectedMenuItem = MenuOption.ACTION_GROUPS;
+    }
 
-	private void showActionGroups() {
-		devicesMenu.setSelected(false);
-		actionGroupsMenu.setSelected(true);
-		changeFragment(new ActionGroupListFragment());
-		selectedMenuItem = MenuOption.ACTION_GROUPS;
-	}
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("selectedMenuItem", selectedMenuItem.toString());
+    }
 
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putString("selectedMenuItem", selectedMenuItem.toString());
-	}
+    private void changeFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame, fragment, "Fragment");
+        transaction.commit();
+    }
 
-	private void changeFragment(Fragment fragment) {
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		transaction.replace(R.id.main_frame, fragment, "Fragment");
-		transaction.commit();
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) return true;
+        return super.onOptionsItemSelected(item);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (drawerToggle.onOptionsItemSelected(item)) return true;
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
 
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-		drawerToggle.syncState();
-	}
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_device:
+                showDevices();
+                return true;
+            case R.id.menu_action_groups:
+                showActionGroups();
+                return true;
+            case R.id.menu_settings:
+                mNavigationView.setCheckedItem(item.getItemId());
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                return true;
 
-	protected enum MenuOption {
-		DEVICES,
-		ACTION_GROUPS
-	}
+        }
+        return false;
+    }
+
+    protected enum MenuOption {
+        DEVICES,
+        ACTION_GROUPS
+    }
 }
--- app/src/main/java/com/homeki/android/SettingsActivity.java
+++ app/src/main/java/com/homeki/android/SettingsActivity.java
@@ -2,25 +2,31 @@ package com.homeki.android;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class SettingsActivity extends ActionBarActivity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.settings);
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
 
-		ActionBar actionBar = getSupportActionBar();
-		actionBar.setDisplayShowCustomEnabled(true);
-		actionBar.setDisplayHomeAsUpEnabled(true);
-		actionBar.setHomeButtonEnabled(true);
-	}
+        Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() != android.R.id.home) return super.onOptionsItemSelected(item);
-		finish();
-		return true;
-	}
+        if (getSupportActionBar() != null) {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() != android.R.id.home) return super.onOptionsItemSelected(item);
+        finish();
+        return true;
+    }
 }
